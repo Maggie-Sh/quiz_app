5aba93554dc68bbc02c31444c939f409085ebea0
Maggie-Sh <85409473+Maggie-Sh@users.noreply.github.com>
2023-01-12 21:24:07 +0400

    worked on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,32 @@
-import { Logo, Nav } from "./styled";
+import { Logo, Nav, IconLink, Title } from "./styled";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import SvgIcon from "@mui/material/SvgIcon";
 
 const Header = () => {
+  const externalLinks = [
+    {
+      name: "GitHub",
+      to: "https://github.com/Maggie-Sh/quiz_app",
+      icon: <GitHubIcon />,
+    },
+    {
+      name: "LinkedIn",
+      to: "https://www.linkedin.com/in/margarit-shakaryan-843302224/",
+      icon: <LinkedInIcon />,
+    },
+  ];
+
   return (
     <header>
       <Nav>
         <Logo to="/">Quiz game</Logo>
+        {externalLinks?.map((link) => (
+          <IconLink href={link.to} target="_blank">
+            <SvgIcon htmlColor="#fff">{link.icon}</SvgIcon>
+            <Title className="title">{link.name}</Title>
+          </IconLink>
+        ))}
       </Nav>
     </header>
   );
--- src/components/header/styled.ts
+++ src/components/header/styled.ts
@@ -1,11 +1,14 @@
 import styled from "styled-components";
-import { inline_padding } from "../../shared/styled";
+import { df_ac, inline_padding } from "../../shared/styled";
 import { Link } from "react-router-dom";
 
 export const Nav = styled.nav`
   padding: 10px;
   ${inline_padding}
   background-color: #ac3b61;
+  ${df_ac}
+  justify-content: flex-end;
+  gap: 10px;
 `;
 
 export const Logo = styled(Link)`
@@ -14,4 +17,30 @@ export const Logo = styled(Link)`
   font-style: oblique;
   font-weight: 700;
   color: #fff;
+  margin-right: auto;
+`;
+
+export const IconLink = styled.a`
+  width: 30px;
+  height: 30px;
+  & .MuiSvgIcon-root {
+    font-size: 30px;
+  }
+  position: relative;
+  &:hover .title {
+    visibility: visible;
+  }
+`;
+
+export const Title = styled.div`
+  position: absolute;
+  top: 100%;
+  right: 0;
+  border: 1px solid #222;
+  border-radius: 5px;
+  background-color: #fff;
+  padding: 3px 8px;
+  font-size: 14px;
+  color: #222;
+  visibility: hidden;
 `;
